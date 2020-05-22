73e83b85fa27d4bc3534108af8ae0ce7ebd5648d
Sakib Rahman <sakib@getd2.com>
2020-05-22 17:59:40 +0600

    SA | AbsorbPointer Screen changed

--- lib/screens/absorb_pointer_screen.dart
+++ lib/screens/absorb_pointer_screen.dart
@@ -1,14 +1,135 @@
 import 'package:flutter/material.dart';
 
 class AbsorbPointerPractice extends StatelessWidget {
+  static String name = 'AbsorbPointer';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AbsorbPointer Parctice"),
+        title: Text("AbsorbPointer Practice"),
       ),
       body: SafeArea(
-        child: Container(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Normal Button"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Red Button'),
+                  color: Colors.red,
+                  onPressed: () {
+                    print('Red Button clicked!');
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Green Button'),
+                  color: Colors.green,
+                  onPressed: () {
+                    print('Green Button clicked!');
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Blue Button'),
+                  color: Colors.blue,
+                  onPressed: () {
+                    print('Blue Button clicked!');
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Absorb Pointer True Button"),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AbsorbPointer(
+
+                  absorbing: true,
+                  child: Row(
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text('Red Button'),
+                        color: Colors.red,
+                        onPressed: () {
+                          print('Red Button clicked!');
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Green Button'),
+                        color: Colors.green,
+                        onPressed: () {
+                          print('Green Button clicked!');
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Blue Button'),
+                        color: Colors.blue,
+                        onPressed: () {
+                          print('Green Button clicked!');
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+
+            SizedBox(
+              height: 40,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Absorb Pointer False Button"),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AbsorbPointer(
+                  absorbing: false,
+                  child: Row(
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text('Red Button'),
+                        color: Colors.red,
+                        onPressed: () {
+                          print('Red Button clicked!');
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Green Button'),
+                        color: Colors.green,
+                        onPressed: () {
+                          print('Green Button clicked!');
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Blue Button'),
+                        color: Colors.blue,
+                        onPressed: () {
+                          print('Blue Button clicked!');
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
 
     );
