4bc7ef30b4ef8b18de3f00436825330c52227a8b
Sakib Rahman <sakib@getd2.com>
2020-05-22 14:15:34 +0600

    SA | Initial Commit

---
+++ lib/screens/absorb_pointer_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AbsorbPointerPractice extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AbsorbPointer Parctice"),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+
+    );
+  }
+}
